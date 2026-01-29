293b8f4cd0edbe0f7e268eebd669cc7a5c23a491
Jorge Barahona <jb30085@gmail.com>
2026-01-28 23:45:49 -0600

    Data de los productos y Service.

---
+++ src/app/data/product.data.ts
@@ -0,0 +1,40 @@
+import { Product } from "../models/product";
+
+export const products: Product[] = [
+    {
+        id: 1,
+        name: 'Teclado Mecánico Asus RGB',
+        description: 'Teclado mecánico para gaming con retroiluminación RGB personalizable y switches de alta durabilidad.',
+        price: 1000
+    },
+    {
+        id: 2,
+        name: 'Samsung Smart TV LED 75"',
+        description: 'Televisor inteligente Samsung de 75 pulgadas con resolución 4K UHD, HDR y sistema operativo Tizen para una experiencia de entretenimiento inmersiva.',
+        price: 2000
+    },
+    {
+        id: 3,
+        name: 'Sony Camara Video',
+        description: 'Cámara de video Sony Handycam con grabación en 4K, estabilización de imagen avanzada y conectividad Wi-Fi para compartir fácilmente tus videos.',
+        price: 3100
+    },
+    {
+        id: 4,
+        name: 'Corsair Memoria RAM 16 GB  DDR5',
+        description: 'Memoria RAM Corsair Vengeance de 16 GB DDR5, ideal para gaming y tareas intensivas, ofreciendo alta velocidad y rendimiento confiable.',
+        price: 1800
+    },
+    {
+        id: 5,
+        name:'NVIDIA ASUS GeForce RTX 4070 Ti',
+        description: 'Tarjeta gráfica NVIDIA ASUS GeForce RTX 4070 Ti con arquitectura Ada Lovelace, ofreciendo un rendimiento excepcional para gaming en 4K y aplicaciones de creación de contenido.',
+        price: 5000
+    },
+    {
+        id: 6,
+        name: 'CPU Intel Core i9-13900K',
+        description: 'Procesador Intel Core i9-13900K de 13ª generación, con 24 núcleos y 32 hilos, ideal para gaming de alta gama y tareas de productividad intensivas.',
+        price: 4000
+    },
+];
