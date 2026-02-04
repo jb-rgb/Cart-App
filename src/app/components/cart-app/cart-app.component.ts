bcacc062dafd61be9a57709fd2d9f430f2aba97b
Jorge Barahona <jb30085@gmail.com>
2026-02-04 12:46:14 -0600

    Maquetando productos view HTML.

--- src/app/components/cart-app/cart-app.component.ts
+++ src/app/components/cart-app/cart-app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../../services/product.service';
 import { Product } from '../../models/product';
 
@@ -8,8 +8,12 @@ import { Product } from '../../models/product';
   imports: [],
   templateUrl: './cart-app.component.html'
 })
-export class CartAppComponent {
+export class CartAppComponent implements OnInit {
   products: Product[] = [];
 
   constructor(private service: ProductService) {  }
+
+  ngOnInit(): void {
+    this.products = this.service.findAll();
+  }
 }
